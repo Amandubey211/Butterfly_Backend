b47b4c85e4c2f7cca682adcbbc913bf3402cf146
Amandubey211 <109097090+Amandubey211@users.noreply.github.com>
2023-10-06 10:57:21 +0530

    ff

--- Server.js
+++ Server.js
@@ -92,7 +92,7 @@ io.on("connection", (socket) => {
     });
   });
   socket.off("setup", () => {
-    console.log("USER DISCONNECTED");
+    console.log("USER DISCONNECTED");//
     socket.leave(userData.Id);
   });
 });
